04b942914c57728287bcb1452cff3c7c139f3f6a
Dammur <vdammur1@BANL141bbf299.local>
2017-07-27 10:17:14 +0530

    js commit

---
+++ Day1/Arrays.js
@@ -0,0 +1,15 @@
+var arr = [1,2,3,true,"string"]
+console.log(arr[0]);
+var arr2 = []
+
+var langs = ["JS", "Java", "C"]
+
+
+var capatials = []
+capatials["India"] = "Delhi"
+capatials["USA"] = "Washington"
+capatials["France"] = "Paris"
+
+for(var key in capatials){
+  console.log(key, capatials[key]);
+}
---
+++ Day1/heelo.js
@@ -0,0 +1,10 @@
+console.log("Hello JS")
+
+var i = 10
+console.log(i)
+
+i = "string"
+console.log(i)
+
+i = null;
+console.log(i)
