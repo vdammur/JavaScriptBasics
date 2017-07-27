8b8ec713b8a212df3e95a478051a1609836500d7
Dammur <vdammur1@BANL141bbf299.local>
2017-07-27 10:46:37 +0530

    Strings sample

---
+++ Day1/Strings.js
@@ -0,0 +1,24 @@
+// String Interpolation
+let str1 = "hello"
+let name = "sam"
+
+let str2 = `${str1} ${name}`
+console.log(str2);
+console.log(typeof str2);
+
+console.log(str1.length);
+console.log(str1.toUpperCase());
+
+let str3 = str1 + " " + name
+console.log(str3);
+
+let multilinestr = `Hi there !
+How are you?
+I'm doing fine
+`
+console.log(multilinestr);
+
+let trymulti = "Hi"+
+"Hello" +
+"Yay"
+console.log(trymulti);
